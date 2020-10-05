e80ce76a5f6a4f947eb7220226b76b808ad96f4b
Guy <guyg7@mail.afeka.ac.il>
2020-10-05 22:38:03 +0300

    Added user name input and memory + basic notifications

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 const socket = io('http://localhost:8080');
+const onStartButton = document.getElementById('onStart');
+
+const name = prompt('Please enter your name');
+
+socket.emit('new-user-name', name);
+
+onStartButton.addEventListener('click', e => {
+    e.preventDefault();
+    if (name == null) {
+        socket.emit('game-started', `User with socket id ${socket.id} started their game!`);
+    } else {
+        socket.emit('game-started', `User ${name} started their game!`);
+    }
+});
+
+function emitEndGame() {
+    if (name == null) {
+        socket.emit('game-ended', `User with socket id ${socket.id} ended their game`)
+    } else {
+        socket.emit('game-ended', `${name} ended their game`)
+    }
+}
 
 socket.on('chat-message', data => {
     console.log(data)
+});
+
+socket.on('user-connected', name => {
+    console.log(`${name} connected!`)
+});
+
+socket.on('game-started', message => {
+    console.log(message);
+});
+
+socket.on('game-ended', message => {
+    console.log(message);
 });
--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app.use('/style', express.static(__dirname + '/style'));
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/root', express.static(__dirname + '/'));
 
-
-
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('view options', { layout: true } );
+
+const players = {};
 
 
 server.listen(PORT, () => {
@@ -26,6 +26,30 @@ app.get('/', (request, response) => {
 });
 
 io.on('connection', socket => {
-    console.log(`New User connected with socket id ${socket.id}`);
-    socket.emit('chat-message', `Hello SnapONap!`)
+    if (players[socket.id] == null)
+        console.log(`New user connected with socket id ${socket.id}`);
+    else
+        console.log(`New user connected with the name ${players[socket.id]}`)
+    socket.emit('chat-message', `Welcome to SnapONap!`);
+
+    socket.on('game-started', message => {
+        console.log(`Message from client: ${message}`);
+        socket.broadcast.emit('game-started', message);
+    });
+
+    socket.on('game-ended', message => {
+        console.log(`Message from client: ${message}`);
+        socket.broadcast.emit('game-ended', message)
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected')
+    });
+
+    socket.on('new-user-name', name => {
+        players[socket.id] = name;
+        socket.broadcast.emit('user-connected', name)
+    })
+
 });
+
