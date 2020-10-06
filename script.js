3613d9023a3ff18e59c3d21c4f6b10113d1eedf1
Guy <guyg7@mail.afeka.ac.il>
2020-10-06 18:00:35 +0300

    Game rooms integration 1 - users can create and join to rooms

--- script.js
+++ script.js
@@ -1,33 +1,46 @@
 const socket = io('http://localhost:8080');
 const onStartButton = document.getElementById('onStart');
+const roomContainer = document.getElementById('room-container');
 
-const name = prompt('Please enter your name');
 
-socket.emit('new-user-name', name);
+if (onStartButton != null) {
+    const name = prompt('Please enter your name');
+    socket.emit('new-user-name', roomName, name);
 
-onStartButton.addEventListener('click', e => {
-    e.preventDefault();
-    if (name == null) {
-        socket.emit('game-started', `User with socket id ${socket.id} started their game!`);
-    } else {
-        socket.emit('game-started', `User ${name} started their game!`);
-    }
-});
+    onStartButton.addEventListener('click', e => {
+        e.preventDefault();
+        if (name == null || name === "") {
+            socket.emit('game-started', `User with socket id ${socket.id} started their game!`);
+        } else {
+            socket.emit('game-started', `User ${name} started their game!`);
+        }
+    });
 
-function emitEndGame() {
-    if (name == null) {
-        socket.emit('game-ended', `User with socket id ${socket.id} ended their game`)
-    } else {
-        socket.emit('game-ended', `${name} ended their game`)
+    function emitEndGame() {
+        if (name == null || name === "") {
+            socket.emit('game-ended', `User with socket id ${socket.id} ended their game`)
+        } else {
+            socket.emit('game-ended', `${name} ended their game`)
+        }
     }
 }
+
+socket.on('room-created', roomName => {
+    const roomElement = document.createElement('div');
+    roomElement.innerText = roomName;
+    const roomLink = document.createElement('a');
+    roomLink.href = `/${roomName}`;
+    roomLink.innerText = 'Join';
+    roomContainer.append(roomElement);
+    roomContainer.append(roomLink);
+});
 
 socket.on('chat-message', data => {
     console.log(data)
 });
 
-socket.on('user-connected', name => {
-    console.log(`${name} connected!`)
+socket.on('user-connected', (room, name) => {
+    console.log(`${name} connected to ${room} room!`)
 });
 
 socket.on('game-started', message => {
--- server.js
+++ server.js
@@ -3,18 +3,22 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const PORT = process.env.PORT || 8080;
+const MAX_ROOMS = 3;
 
 
 app.use('/js', express.static(__dirname + '/js'));
 app.use('/style', express.static(__dirname + '/style'));
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/root', express.static(__dirname + '/'));
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('view options', { layout: true } );
 
+
 const players = {};
+const rooms = { defaultRoom: {} };
 
 
 server.listen(PORT, () => {
@@ -22,7 +26,28 @@ server.listen(PORT, () => {
 });
 
 app.get('/', (request, response) => {
-    response.render('index');
+    response.render('index', {rooms: rooms});
+});
+
+app.get('/:room', (request, response) => {
+    if (rooms[request.params.room] != null)
+        response.render('room', { roomName: request.params.room })
+    else
+        response.redirect('/')
+});
+
+app.post('/room', (request, response) => {
+    if (request.body.room !== "" && request.body.room != null) {
+        if (rooms[request.body.room] != null || rooms.length >= MAX_ROOMS) {
+            return response.redirect('/')
+        }
+        rooms[request.body.room] = { players: {} };
+        response.redirect(request.body.room)
+        // Emit new message that a new room was created
+        io.emit('room-created', request.body.room)
+    } else {
+        return response.redirect('/')
+    }
 });
 
 io.on('connection', socket => {
@@ -43,12 +68,17 @@ io.on('connection', socket => {
     });
 
     socket.on('disconnect', () => {
-        console.log('User disconnected')
+        if (players[socket.id] != null && players[socket.id] !== "")
+            console.log(`${players[socket.id]} disconnected`)
+        else
+            console.log(`User with socket id ${socket.id} disconnected`)
+        delete players[socket.id];
     });
 
-    socket.on('new-user-name', name => {
+    socket.on('new-user-name', (room, name) => {
+        console.log(`room name: ${room}`)
         players[socket.id] = name;
-        socket.broadcast.emit('user-connected', name)
+        socket.broadcast.emit('user-connected', room, name)
     })
 
 });
